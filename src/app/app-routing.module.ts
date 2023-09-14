d9157516ee719f62921183dbd9db1434e3736cd0
Shkarupa Daniil <zshkarrr@outlook.com>
2023-09-14 11:24:58 +0300

    Revert "Add text-transform pipe"
    This reverts commit 00bd241ce5b58a9493eae4e4e3a6fa55cd493b42.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-    {
-        path: 'pipes',
-        loadChildren: () =>
-            import('./pipes-flow/pipes.module').then((m) => m.PipesModule),
-    },
-    {
-        path: '**',
-        redirectTo: 'pipes',
-    },
-];
+const routes: Routes = [];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    title = 'learning';
+  title = 'learning';
 }
